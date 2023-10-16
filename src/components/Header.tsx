bfbfb22b05eafdfec963124544c3a54203b4f9bc
Ahsan Imam <aimam@achieve.com>
2023-10-16 15:09:15 -0600

    Update host

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
 // import VisitIdIframe from "./VisitIdIframe"
+import { useEffect } from 'react'
+import { hosts } from '../constants/hosts'
+import { setCookieValue } from '../utils/cookies'
 import { Outlet } from 'react-router-dom';
 
 const Header = () => {
+  useEffect(() => {
+    window.addEventListener('message', function (event) {
+      const host = event.origin;
+      console.log(`Setting cookie ${host}`)
+      // Only take action if origin is present and visitId is present
+      if (event.data.eh_visit_id && hosts.includes(host)) {
+        setCookieValue(`eh_visit_id`, event.data.eh_visit_id, {
+          sameSite: 'strict'
+        });
+        setCookieValue(`eh_visit_ts`, event.data.eh_visit_ts, {
+          sameSite: 'strict'
+        });
+      }
+    })
+  }, [])
   return (
     <>
-      <h1>Header Iframe I</h1>
-      {/* <VisitIdIframe /> */}
+      <h1>Github Host</h1>
       <Outlet />
     </>
   )
