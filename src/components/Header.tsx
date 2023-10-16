af92716455f47269fd6822e3a501a5a588fee3e8
Ahsan Imam <aimam@achieve.com>
2023-10-16 15:49:27 -0600

    Update host

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,11 @@ import { Outlet } from 'react-router-dom';
 const Header = () => {
   useEffect(() => {
     window.addEventListener('message', function (event) {
-      console.log('Header event', event)
       const host = event.origin;
-      console.log(`Setting cookie ${host}`)
 
       // Only take action if origin is present and visitId is present
       if (event.data.eh_visit_id && hosts.includes(host)) {
+        console.log(`Setting cookie ${host}`)
         setCookieValue(`eh_visit_id`, event.data.eh_visit_id, {
           sameSite: 'strict'
         });
