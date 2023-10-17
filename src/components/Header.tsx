58ce2f8910decf7a1c05cd01e3b545e63d05a3f5
Ahsan Imam <aimam@achieve.com>
2023-10-17 09:36:44 -0600

    Update host

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,6 +19,7 @@ const Header = () => {
         setCookieValue(`eh_visit_ts`, event.data.eh_visit_ts, {
           sameSite: 'lax'
         });
+        window.sessionStorage.setItem('eh_visit_id', event.data.eh_visit_id)
       }
     })
   }, [])
